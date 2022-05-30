d5dc9834c6c3c4893b927c855d260ba18d01486a
CBoensch <cboensch6505@gmail.com>
2022-05-30 18:59:14 -0400

    Started Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className='container header__container'></div>
+			<h1>Raised Frame</h1>
+			<h5>Elevate Your Footage</h5>
+			<h5 className='text-light'>Certified Drone Sevices</h5>
+		</header>
+	);
 };
 
 export default Header;
