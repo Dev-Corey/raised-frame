26d4e885fb4e788096c765ea955c64bce9f890df
CBoensch <cboensch6505@gmail.com>
2022-06-06 16:55:04 -0400

    Nav started, Icons imported from react-icons lib

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,18 +8,17 @@ const Header = () => {
 	return (
 		<header>
 			<div className='container'>
-				<div className='overlay'>
-					<video src={mainhero} autoPlay loop muted />
-					<div className='header__container'>
-						<h1>Raised Frame</h1>
-						<h5>Elevate Your Footage</h5>
-						<h5 className='text-light'>Certified Drone Sevices</h5>
-						<CTA />
-						<HeaderSocials />
-						<a href='#contact' className='scroll__down'>
-							Scroll Down
-						</a>
-					</div>
+				<div className='overlay'></div>
+				<video src={mainhero} autoPlay loop muted />
+				<div className='header__container'>
+					<h1>Raised Frame</h1>
+					<h5>Elevate Your Footage</h5>
+					<h5 className='text-light'>Certified Drone Sevices</h5>
+					<CTA />
+					<HeaderSocials />
+					<a href='#contact' className='scroll__down'>
+						Scroll Down
+					</a>
 				</div>
 			</div>
 		</header>
