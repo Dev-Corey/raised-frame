77ccf2e94031ebb93496fe254ea5ee14b2fbb422
CBoensch <cboensch6505@gmail.com>
2022-06-11 16:21:26 -0400

    Experience Section Started

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./experience.css";
 
 const Experience = () => {
-	return <section id='experience'>Experience</section>;
+	return (
+		<section id='experience'>
+			<h5>What Skills I Have</h5>
+			<h2>My Experience</h2>
+
+			<div className='container experience__container'>
+				<div className='experience__frontend'></div>
+				<div className='experience__backend'></div>
+			</div>
+		</section>
+	);
 };
 
 export default Experience;
